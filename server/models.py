4c880ce803c44e3f0a23857d268ea5d9497385da
Franklin Hau <fhauchi@gmail.com>
2023-09-03 17:48:49 -0600

    Flask-SQLAlchemy lab

--- server/models.py
+++ server/models.py
@@ -9,15 +9,23 @@ db = SQLAlchemy(metadata=metadata)
 
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
-
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    birthday = db.Column(db.Date)
+    animals = db.relationship('Animal', backref = 'zookeeper', lazy = True)
+
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
-
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(80))
+    open_to_visitors = db.Column(db.Boolean)
+    animals = db.relationship('Animal', backref = 'enclosure', lazy = True)
 
 class Animal(db.Model):
     __tablename__ = 'animals'
-
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    species = db.Column(db.String(80))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
